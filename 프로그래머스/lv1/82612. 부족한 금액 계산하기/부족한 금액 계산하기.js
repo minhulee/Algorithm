122866ce65eb4c24940495b35fb78cbd70c7d507
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-03-07 22:06:51 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let pay = 0;
+    
+    for(let i = 1; i <= count; i++) {
+        pay += price * i;    
+    }
+    
+    return money - pay < 0 ? -(money - pay) : 0;
+}
