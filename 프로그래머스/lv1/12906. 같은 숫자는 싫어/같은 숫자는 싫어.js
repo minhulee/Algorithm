7fc4728fc280058625a3b4c81d8dfd62fc9f2c8e
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-03-08 10:43:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.71 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,4 @@
 function solution(arr)
 {
-    const answer = [arr[0]];
-    
-    for(let i = 0; i < arr.length - 1; i++) {
-        if(arr[i] !== arr[i+1]) {
-            answer.push(arr[i+1]);
-        }
-    }
-    
-    return answer;
-// return arr.filter((el, idx) => el !== arr[idx + 1]);
+    return arr.filter((el, idx) => el !== arr[idx + 1]);
 }
