02dda623a0cc2134cef26866676350c2c89ff667
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-03-15 00:13:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,6 @@
 function solution(s) {
-    // return s.toLowerCase().split(" ").map(e => {
-    //     return e[0].toUpperCase() + e.substring(1);
-    // }).join(" ");
-    
-    return s.toLowerCase().split(" ").map((v)=> v.charAt(0).toUpperCase()+v.substring(1)).join(" ");
+    return s.split(" ").map(el => {
+        console.log(el);
+        return `${el[0].toUpperCase()}${el.slice(1).toLowerCase()}`;
+    }).join(" ");
 }
--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 0.0 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
