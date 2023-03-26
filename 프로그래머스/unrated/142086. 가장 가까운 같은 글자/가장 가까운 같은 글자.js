7a0044454d014abfb36028ef4699eb6eefc7996d
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-03-26 19:57:08 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.35 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,15 +1,24 @@
 function solution(s) {
-    const result = [];
-    const loaded = [];
+//     const result = [];
+//     const loaded = [];
     
-    for(let i = 0; i < s.length; i++) {
-        const loadedIdx = loaded.lastIndexOf(s[i])
+//     for(let i = 0; i < s.length; i++) {
+//         const loadedIdx = loaded.lastIndexOf(s[i])
         
-        if(loadedIdx > -1) result.push(i - loadedIdx);
-        else result.push(-1);
+//         if(loadedIdx > -1) result.push(i - loadedIdx);
+//         else result.push(-1);
         
-        loaded.push(s[i]);
-    } 
+//         loaded.push(s[i]);
+//     } 
     
-    return result;
+//     return result;
+    
+    const hash = {};
+
+    return [...s].map((v,i)=>{
+        let result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        
+        return result;
+    });
 }
