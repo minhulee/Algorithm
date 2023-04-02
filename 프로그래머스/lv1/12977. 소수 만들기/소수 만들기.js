89ad70bf7529cc4ee4697e5e0172305954299cef
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-03 08:43:14 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,17 +1,18 @@
 function solution(nums) {
     let count = 0;
     
+    const primeCheck = (n) => {
+        for(let d = 2; d < Math.sqrt(n) + 1; d++) {
+            if(n % d === 0) return false;
+        }
+        
+        return true;
+    }
+    
     for(let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j++) {
             for (let k = j + 1; k < nums.length; k++) {
-                let isPrime = true;
-                const combie = nums[i] + nums[j] + nums[k];
-                
-                for(let devide = 2; devide < combie; devide++) {
-                    if(combie % devide === 0) isPrime = false;
-                }
-                
-                if(isPrime) count++;
+                if(primeCheck(nums[i] + nums[j] + nums[k])) count++;
             }
         }
     }
