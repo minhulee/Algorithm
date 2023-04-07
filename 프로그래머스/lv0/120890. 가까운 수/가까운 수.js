cda43525428e61c30a4b2290cc7efed239e33152
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-07 20:05:02 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -3,6 +3,6 @@ function solution(array, n) {
     array = array.map(el => [el, Math.abs(el - n)]);
     
     //동일한 차의 수가 존재할 경우 가장 작은 수를 구한다.
-    const min = Math.min(...array.map(el => el[1]))  
+    const min = Math.min(...array.map(el => el[1]))
     return array.filter(el => el[1] === min).sort((a, b) => a[0] - b[0])[0][0];
 }
