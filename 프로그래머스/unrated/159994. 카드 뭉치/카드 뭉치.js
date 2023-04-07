6adfcedc9fb59153e01b2a10dc46024e77dceb5a
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-07 15:54:36 +0900

    [unrated] Title: 카드 뭉치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    let cur = 0;
+    let select1 = 0;
+    let select2 = 0;
+    
+    while(cur !== goal.length) {
+        if(goal[cur] === cards1[select1]) {
+            select1++;
+        }else if(goal[cur] === cards2[select2]) {
+            select2++;
+        } else return "No";
+        
+        cur++
+    }
+    
+    return "Yes";
+}
