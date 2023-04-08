2e83bcb290ce1912ee4aead6f28d475660502ebe
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-08 23:57:21 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,5 +1,5 @@
 function solution(emergency) {
-    return emergency.map((e, i) => [i, e]).sort((a, b) => b[1] - a[1])
-                    .map((e, i) => [...e, i + 1]).sort((a, b) => a[0] - b[0])
-                    .map(e => e[2]);   
+    const sorted = [...emergency].sort((a, b)=> b - a);
+    
+    return emergency.map(e => sorted.indexOf(e) + 1);
 }
