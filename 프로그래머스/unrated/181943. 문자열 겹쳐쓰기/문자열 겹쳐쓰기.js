baebdf9cb35c2df7d5da40203d224786729e50ae
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-24 23:08:49 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,6 +1,8 @@
 function solution(my_string, overwrite_string, s) {
-    return my_string.split('').map((c, idx) => {
-        if(overwrite_string[idx - s]) return overwrite_string[idx - s];
-        else return c;
-    }).join('');
+    // return my_string.split('').map((c, idx) => {
+    //     if(overwrite_string[idx - s]) return overwrite_string[idx - s];
+    //     else return c;
+    // }).join('');
+
+    return `${my_string.slice(0, s)}${overwrite_string}${my_string.slice(overwrite_string.length + s)}`
 }
