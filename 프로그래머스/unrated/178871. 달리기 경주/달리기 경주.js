49623d8a1e9516470c3f8c27b7ef92aaaf7323a3
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-04-27 22:46:54 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,13 +1,22 @@
 function solution(players, callings) {
-    const rank = new Map(players.map((el, idx) => [el, idx]));
-    
-    for(let i = 0; i < callings.length; i++) {
-        const idx = rank.get(callings[i]);
+    const playerMap = {};
+  
+    // calling 원소들의 idx를 미리 구해서 저장
+    for (let i = 0; i < players.length; i++) {
+        playerMap[players[i]] = i;
+    }
+
+    for (let i = 0; i < callings.length; i++) {
+        const idx = playerMap[callings[i]];
         const temp = players[idx-1];
-        
-        [players[idx], players[idx - 1]] = [players[idx -1], players[idx]];
-        rank.set(callings[i], rank.get(callings[i]) - 1);
-        rank.set(temp, idx);
+      
+        // 해당 idx와 이전 idx의 원소를 swap
+        players[idx-1] = callings[i];
+        players[idx] = temp;
+      
+        // map의 idx도 갱신
+        playerMap[callings[i]] = idx - 1;
+        playerMap[temp] = idx;
     }
     
     return players;
