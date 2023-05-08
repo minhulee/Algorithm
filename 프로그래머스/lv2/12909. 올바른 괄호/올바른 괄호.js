f4f1aaea1000b4c733515ee98e8e55fe8c37cdbb
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-08 10:40:40 +0900

    [level 2] Title: 올바른 괄호, Time: 2.55 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,22 +1,21 @@
 function solution(s){
-    const stack = [];
-    
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === '(') stack.push('(');
-        else if(stack.length !== 0) stack.pop();
-        else return false;
-    }
-    
-    return stack.length === 0;
-
-// function solution(s){
-//     let count = 0;
+//     const stack = [];
     
 //     for(let i = 0; i < s.length; i++) {
-//         if(s[i] === '(') count++;
-//         else if(count !== 0) count--;
+//         if(s[i] === '(') stack.push('(');
+//         else if(stack.length !== 0) stack.pop();
 //         else return false;
 //     }
     
-//     return count === 0;
+//     return stack.length === 0;
+
+    let count = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(count === -1) return false;
+        if(s[i] === '(') count++;
+        else count--;
+    }
+    
+    return count === 0;
 } 
