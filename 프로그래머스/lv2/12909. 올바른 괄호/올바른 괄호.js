fababb1f52c73a32c52fac95e39a384d40657be0
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-08 10:49:45 +0900

    [level 2] Title: 올바른 괄호, Time: 2.68 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,19 +1,9 @@
-function solution(s){
-//     const stack = [];
-    
-//     for(let i = 0; i < s.length; i++) {
-//         if(s[i] === '(') stack.push('(');
-//         else if(stack.length !== 0) stack.pop();
-//         else return false;
-//     }
-    
-//     return stack.length === 0;
-
+function solution(s) {
     let count = 0;
     
     for(let i = 0; i < s.length; i++) {
         if(count === -1) return false;
-        if(s[i] === '(') count++;
+        else if(s[i] === '(') count++;
         else count--;
     }
     
