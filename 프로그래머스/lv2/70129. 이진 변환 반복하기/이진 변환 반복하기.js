f260af5a65470efac884d880e7664aa1112d368f
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-08 10:42:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.83 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -2,7 +2,7 @@ function solution(s) {
     const result = [0, 0]
     
     while(s !== "1") {
-        result[1] += parseInt(s.split("0").length - 1);
+        result[1] += s.split("0").length - 1;
         s = s.replaceAll("0", "").length.toString(2);
         result[0]++;
     }
