5fc690a10a30992f1e0873392fcd1374121c2250
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-08 10:29:45 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.74 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,6 +1,6 @@
 function solution(A,B){
-    const sortedA = A.sort((a,b) => a - b);
-    const sortedB = B.sort((a,b) => b - a);
+    A.sort((a,b) => a - b);
+    B.sort((a,b) => b - a);
     
-    return sortedA.reduce((acc, cur, idx) => acc += cur * sortedB[idx], 0);
+    return A.reduce((acc, cur, idx) => acc += cur * B[idx], 0);
 }
