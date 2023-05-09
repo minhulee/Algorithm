8108a75ea66858ef0e89ee50154d1bfb30136579
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-09 17:48:14 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,7 +1,4 @@
 function solution(n) {
     const count = n.toString(2).match(/1/g).length
-    
-    while(n++) {
-        if(count === n.toString(2).match(/1/g).length) return n;
-    }
+    while(n++) if(count === n.toString(2).match(/1/g).length) return n;
 }
