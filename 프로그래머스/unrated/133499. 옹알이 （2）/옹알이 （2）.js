fd195eae0b65ec3754313bf5838ce7e56ad42177
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-11 08:44:28 +0900

    [unrated] Title: 옹알이 (2), Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -7,6 +7,7 @@ function solution(babbling) {
         let s = filter.find(e => t.includes(e))
         
         while(s) {
+            // 문자열이 중복되는 경우, 가능한 발음을 제외하고 합친 문자열이 발음이 되는 경우
             if(t.includes(s.repeat(2))) break;
             t = t.replace(s, ' ');
             s = filter.find(e => t.includes(e));
