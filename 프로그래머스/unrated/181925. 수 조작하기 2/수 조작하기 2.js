6a361d9766ec45b4f45157a56784b95bec87a29e
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-05-29 04:44:56 +0900

    [unrated] Title: 수 조작하기 2, Time: 8.30 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,12 +1,4 @@
 function solution(numLog) {
-    return numLog.reduce((a, c, i) => {
-        if(numLog[i+1] === undefined) return a;
-        
-        const diff = numLog[i+1] - numLog[i];
-        
-        if(diff === 1) return a += 'w';
-        if(diff === -1) return a += 's';
-        if(diff === 10) return a += 'd';
-        if(diff === -10) return a += 'a';  
-    }, '')
+    const filter = { '1': 'w', '-1': 's', '10': 'd', '-10': 'a' };
+    return numLog.slice(1).map((v, i) => filter[v - numLog[i]]).join('')
 }
