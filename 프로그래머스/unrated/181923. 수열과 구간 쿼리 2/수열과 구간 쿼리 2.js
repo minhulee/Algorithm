883cf6c787fb96f6f3e9b24881470a1932523c3f
Minhun Lee <110901252+lmimoh@users.noreply.github.com>
2023-06-10 23:52:03 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 12.89 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,8 +1,8 @@
 function solution(arr, queries) {
     return queries.map(q => {
         const [s, e, k] = q;
-        // const result = arr.filter((n, i) => s <= i && i <= e && n > k).sort((a, b) => a-b)[0];
-        const result = arr.slice(s, e+1).sort((a,b) => a-b).find(n => n > k);
+        const result = arr.filter((n, i) => s <= i && i <= e && n > k).sort((a, b) => a-b)[0];
+        // const result = arr.slice(s, e+1).sort((a,b) => a-b).find(n => n > k);
         
         return result ? result : -1;
     })
