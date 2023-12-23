72f0bc9cd9ec3f1227d93fdea76e261112853c06
이민훈 <110901252+minhulee@users.noreply.github.com>
2023-12-23 15:11:28 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    
+    while(1)
+    {
+        scanf("%d %d", &a, &b);
+        if (!a && !b)
+            break ;
+        printf("%d\n", a + b);
+    }
+    return (0);
+}
