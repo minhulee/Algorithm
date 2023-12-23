bf61ff402cb97394da6acc368250db4ade81288b
이민훈 <110901252+minhulee@users.noreply.github.com>
2023-12-23 14:45:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int byte;
+    
+    scanf("%d", &byte);
+    for(int i = 0; i < byte / 4; i++)
+        printf("long ");
+    printf("int");
+    return (0);
+}
