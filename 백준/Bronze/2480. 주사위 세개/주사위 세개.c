cb78bb6f712bd7fbff7e6ce85690f1f6a40d3930
이민훈 <110901252+minhulee@users.noreply.github.com>
2023-12-23 13:10:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    if ((a == b) && (b == c))
+        printf("%d", 10000 + a * 1000);
+    else if (a == b || a == c)
+        printf("%d", 1000 + a * 100);
+    else if (b == c)
+        printf("%d", 1000 + b * 100);
+    else
+        printf("%d", a > b ? (a > c ? a * 100 : c * 100) : (b > c ? b * 100 : c * 100));
+    return (0);
+}
