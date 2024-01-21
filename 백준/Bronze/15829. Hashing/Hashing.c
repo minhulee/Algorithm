59dac2dcd0de1203ed960494b9e89d840e65da43
이민훈 <110901252+minhulee@users.noreply.github.com>
2024-01-21 19:00:50 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
+#define  MOD 1234567891
 
-int	square(int n)
-{
-	int res = 1, base = 31;
-	for (int i = 0; i < n; i++)
-		res *= base;
-	return (res);
-}
-
-int main(void)
-{
-    int t, total = 0;
-    scanf("%d", &t);
-    
-    char s[t];
-    scanf("%s", s);
-    
-    for (int i = 0; i < t; i++)
-        total += (s[i] - 96) * square(i);
-    printf("%d", total);
-    return (0);
+int main(){
+	int t;
+	long long result = 0, r = 1;
+	char str[51];
+	
+	scanf("%d %s", &t, str);
+	for (int i = 0; i < t; i++)
+    {
+		result = (result + (str[i] - 96) * r) % MOD;
+		r = (r*31) % MOD;
+	}
+	printf("%lld", result);
 }
