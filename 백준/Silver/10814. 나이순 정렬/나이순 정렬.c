733e2a993c8a353bc44c05fc6ba4032b4db00f0e
이민훈 <110901252+minhulee@users.noreply.github.com>
2024-02-20 10:52:31 +0900

    [Silver V] Title: 나이순 정렬, Time: 56 ms, Memory: 12420 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.c
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct s_data
-{
-	int  age;
-	char name[101];
-} t_data;
 
 int comp(const void *arg1, const void *arg2)
 {
-	t_data *a = (t_data *)arg1;
-	t_data *b = (t_data *)arg2;
+	int *a = (int *)arg1;
+	int *b = (int *)arg2;
     
-	if (a->age > b->age)
+	if (a[1] > b[1])
         return 1;
-	else if (a->age < b->age) 
+	else if (a[1] < b[1]) 
         return -1;
 	else
         return 0; 
@@ -22,21 +15,20 @@ int comp(const void *arg1, const void *arg2)
 
 int main(void)
 {
-	int    N;
-    t_data *datas;
-    
+	int N;
+
 	scanf("%d", &N);
-    datas = (t_data *)malloc(sizeof(t_data) * N);
-    
+	int		idx[N][2];
+	char	s[N][101];
+
 	for (int i = 0; i < N; i++)
-		scanf("%d %s", &datas[i].age, datas[i].name);
-    
-	qsort(datas, N, sizeof(t_data), comp);
-    
+	{
+		scanf("%d %s", &(idx[i][1]), s[i]);
+		idx[i][0] = i;
+	}
+	
+	qsort(idx, N, sizeof(idx) / N, comp);
+
 	for (int i = 0; i < N; i++)
-		printf("%d %s\n", datas[i].age, datas[i].name);
-    
-	free(datas);
-    
-	return (0);
+		printf("%d %s\n", idx[i][1], s[(idx[i][0])]);
 }
