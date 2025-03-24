18efb54b24cbb41eaa321b4e54b3a45747bb36f8
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-03-24 14:20:21 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cpp
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int solution(vector<vector<int>> board, int k) {
     int answer = 0;
-    for (int i = 0; board.begin() + i != board.end(); i++)
+    for (int i = 0; i < board.size(); i++)
     {
-        for (int j = 0; (i + j <= k) && (board[i].begin() + j != board[i].end()); j++)
-             answer += board[i][j];
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            if (i + j <= k)
+                answer += board[i][j];
+            else
+                break ;
+        }
     }
     return answer;
 }
