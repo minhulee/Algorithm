e8d8e3d523ff6b002d273ddb1d39579e6dc6d88e
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-03-27 15:26:52 +0900

    [level 2] Title: 점 찍기, Time: 13.06 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,15 +1,8 @@
 function solution(k, d) {
     var answer = 0;
     
-    for (let i = 0; i * k <= d; i++) {
-        let x = i * k;
-        
-        let maxY = Math.floor(Math.sqrt(d * d - x * x));
-        
-        if (maxY >= 0) {
-            answer += Math.floor(maxY / k) + 1;
-        }
-    }
+    for (let x = 0; x <= d; x += k)
+        answer += Math.floor(Math.floor(Math.sqrt(d * d - x * x)) / k) + 1;
     
     return answer;
 }
