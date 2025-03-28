8349515452411ee70e79bdd47b17e95b389d74bd
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-03-28 15:39:56 +0900

    [level 0] Title: 세 개의 구분자, Time: 27.08 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,18 +1,4 @@
 function solution(myStr) {
-    var answer = [];
-    let buffer = "";
-    for (let c of myStr)
-    {
-        if (c == 'a' || c == 'b' || c == 'c')
-        {
-            if (buffer.length != 0)
-                answer.push(buffer);
-            buffer = "";
-            continue ;
-        }
-        buffer += c;
-    }
-    if (buffer.length != 0)
-        answer.push(buffer);
-    return !answer.length ? ["EMPTY"] : answer; 
+    var answer = myStr.split(/[abc]+/).filter((e) => e.length > 0);
+    return answer.length == 0 ? ["EMPTY"] : answer;
 }
