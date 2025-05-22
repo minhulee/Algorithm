aaa4654b1774a2ddccd0242f3bb53368514c8950
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-05-22 15:08:42 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.38 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,16 +1,16 @@
 function solution(arr) {
-    // 정사각형이 되기 위한 w 계산
-    const w = Math.max(arr.length, ...arr.map((e) => e.length))
-    
-    // 행 생성
-    let res = arr.map((e) => {
-        if (e.length == w)
-            return e;
-        return e.concat(new Array(w - e.length).fill(0));
-    })
-    
-    // 열 생성
-    if (res.length == w)
-        return (res);
-    return (res.concat(new Array(w - res.length).fill(new Array(w).fill(0))))
+    // 정사각형의 길이 게산
+    const w = Math.max(arr.length, ...arr.map(e => e.length));
+
+    // 행 추가
+    const padded = arr.map((e) => {
+        return e.concat(new Array(w - e.length).fill(0))
+    });
+
+    // 열 추가 -> 각 요소가 참조되지 않도록 from()으로 생성
+    const padRows = Array.from({ length: w - padded.length }, () => {
+        return new Array(w).fill(0);
+    });
+
+    return padded.concat(padRows);
 }
