7e0897b02772374257cf7c0bd1ec42d242bcfeb9
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-05-26 13:27:09 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,11 +1,4 @@
 function solution(rank, attendance) {
-    // array + index 보다는 obj + property를 통해서 코드 가독성 향상
-    const attended = rank
-        .map((rankValue, idx) => ({ rank: rankValue, index: idx }))
-        .filter(({ index }) => attendance[index])
-        .sort((a, b) => a.rank - b.rank);
-
-    // idx를 통한 배열 요소 참조보다는 요소를 값복사 하여 사용
-    const [first, second, third] = attended;
-    return 10000 * first.index + 100 * second.index + third.index;
+    const sorted = rank.map((e, i) => [e, i]).filter((e, i) => attendance[i]).sort((a, b) => a[0] - b[0]);
+    return (10000 * sorted[0][1] + 100 * sorted[1][1] + sorted[2][1]);
 }
