609983d582c492ce1619892001112e5cdd8fa8aa
이민훈 <110901252+minhulee@users.noreply.github.com>
2025-05-27 14:45:42 +0900

    [level 0] Title: 그림 확대, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,8 +1,7 @@
 function solution(picture, k) {
-    const resized = picture.map((e) => {
-        return e.split('').map((e) => {
-            return e.repeat(k);
-        }).join('');
-    }).flatMap((e) => Array(k).fill(e));
-    return resized;
+    // 문자열은 불변하므로 Array를 통해 생성된 배열이 동일한 문자열 resized를 참조해도 OK!
+    return picture.flatMap(line => {
+        const resized = [...line].map(c => c.repeat(k)).join('');
+        return Array(k).fill(resized);
+    });
 }
